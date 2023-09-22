eaa7bde00c5494b8df33d636ac4d9d613205de22
syckor <syckor@naver.com>
2023-09-22 23:27:20 +0900

    create ingda project

---
+++ src/main/java/com/example/ingda/IngdaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ingda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngdaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ingda/IngdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ingda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
