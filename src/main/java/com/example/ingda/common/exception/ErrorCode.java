bfa97c8199358daf406370e2616d0cdec39d6dc0
syckor <syckor@naver.com>
2023-10-10 22:18:07 +0900

    과거 1년치 일기 조회, 일기 상세 조회, 일기 수정

--- src/main/java/com/example/ingda/common/exception/ErrorCode.java
+++ src/main/java/com/example/ingda/common/exception/ErrorCode.java
@@ -8,6 +8,7 @@ import lombok.Getter;
 public enum ErrorCode {
 
     SERVER_ERROR("알 수 없는 서버 에러가 발생했습니다.", 500),
+    AUTH_FAIL("해당 작업에 대한 권한이 없습니다.", 403),
     FORBIDDEN_ERROR("서버 사용 권한이 없습니다.",403),
     TOKEN_ERROR("토큰이 유효하지 않습니다.",401),
     MEMBER_NOT_FOUND("알 수 없는 사용자입니다.", 400),
@@ -18,7 +19,8 @@ public enum ErrorCode {
     INACTIVE_MEMBER("비활성화 계정입니다.", 400),
     ACTIVATION_FAILED("계정 활성화에 실패하였습니다.", 400),
     VERIFYING_CODE_WRONG("이메일 인증에 실패하였습니다.", 400),
-    DIARY_COUNT_LIMIT("일기는 하루에 한 번만 쓰기가 가능합니다.", 400);
+    DIARY_COUNT_LIMIT("일기는 하루에 한 번만 쓰기가 가능합니다.", 400),
+    DIARY_NOT_FOUND("일기에 대한 정보를 찾을 수 없습니다.", 404);
 
     private final String msg;
     private final int statusCode;
--- src/main/java/com/example/ingda/diary/controller/DiaryController.java
+++ src/main/java/com/example/ingda/diary/controller/DiaryController.java
@@ -3,15 +3,15 @@ package com.example.ingda.diary.controller;
 import com.example.ingda.common.MessageCode;
 import com.example.ingda.common.ResponseMessage;
 import com.example.ingda.diary.dto.DiaryRequestDto;
+import com.example.ingda.diary.dto.DiaryResponseDto;
 import com.example.ingda.diary.service.DiaryService;
 import com.example.ingda.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +23,24 @@ public class DiaryController {
     public ResponseMessage<?> writeDiary(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody @Valid  DiaryRequestDto diaryRequestDto){
         diaryService.writeDiary(userDetails.getMember(), diaryRequestDto);
         return new ResponseMessage<>(MessageCode.SUCCESS, null);
+    }
+
+    @GetMapping("/diaries")
+    public ResponseMessage<List<DiaryResponseDto>> getDiariesLastOneYear(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        return new ResponseMessage<>(MessageCode.SUCCESS, diaryService.getDiariesLastOneYear(userDetails.getMember()));
+    }
+
+    @GetMapping("/diaries/{diaryId}")
+    public ResponseMessage<DiaryResponseDto> getDiaryDetail(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                                @PathVariable(name="diaryId") Long diaryId){
+        return new ResponseMessage<>(MessageCode.SUCCESS, diaryService.getDiaryDetail(userDetails.getMember(), diaryId));
+    }
+
+    @PutMapping("/diaries/{diaryId}")
+    public ResponseMessage<DiaryResponseDto> modifyDiaryContents(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                            @PathVariable(name="diaryId") Long diaryId,
+                                                                 @RequestBody DiaryRequestDto diaryRequestDto){
+        diaryService.modifyDiaryContents(userDetails.getMember(), diaryId, diaryRequestDto);
+        return new ResponseMessage<>(MessageCode.SUCCESS, null );
     }
 }
