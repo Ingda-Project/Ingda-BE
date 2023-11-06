c6d8b034d1bfe266364eafbe817364ad74eca6bd
syckor <syckor@naver.com>
2023-11-06 23:26:42 +0900

    회원 생성 백도어 api 권한 풀기

--- src/main/java/com/example/ingda/security/config/WebSecurityConfig.java
+++ src/main/java/com/example/ingda/security/config/WebSecurityConfig.java
@@ -52,7 +52,9 @@ public class WebSecurityConfig {
                 .antMatchers(HttpMethod.POST, new String[]{"/member", "/login", "/email", "/email/verifying"}).permitAll()
                 .antMatchers(HttpMethod.GET, new String[]{"/member/nickname"}).permitAll()
                 .antMatchers(HttpMethod.PUT, new String[]{"/member/activation"}).permitAll()
+                .antMatchers("/admin/member").permitAll()
                 .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+
                 .anyRequest().authenticated()
                 .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
         http.exceptionHandling()
