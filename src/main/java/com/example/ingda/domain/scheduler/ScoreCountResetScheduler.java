dd6379f17657a08cd360ba8d24e21360f7a890dd
syckor <syckor@naver.com>
2023-11-14 23:33:00 +0900

    스케줄러 선언 및 ScoreCount 초기화

---
+++ src/main/java/com/example/ingda/domain/scheduler/ScoreCountResetScheduler.java
@@ -0,0 +1,23 @@
+package com.example.ingda.domain.scheduler;
+
+import com.example.ingda.domain.score.repository.ScoreRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ScoreCountResetScheduler {
+
+    private final ScoreRepository scoreRepository;
+
+    @Transactional
+    @Scheduled(cron = "0 0 4 * * ?")
+    public void scoreCountReset(){
+        log.info(">>> score count reset start <<<");
+        scoreRepository.resetScoreCounts();
+    }
+}
--- src/main/java/com/example/ingda/domain/score/repository/ScoreRepository.java
+++ src/main/java/com/example/ingda/domain/score/repository/ScoreRepository.java
@@ -3,9 +3,13 @@ package com.example.ingda.domain.score.repository;
 import com.example.ingda.domain.member.entity.Member;
 import com.example.ingda.domain.score.entity.Score;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ScoreRepository extends JpaRepository<Score, Member> {
-
+    @Modifying()
+    @Query("update Score s set s.diaryCount = 1, s.loginCount = 1")
+    void resetScoreCounts();
 }
